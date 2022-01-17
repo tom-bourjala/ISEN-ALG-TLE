dc5cce50818499854c24407c8c0c72abe91c5637
William <willdk13@gmail.com>
2022-01-17 13:11:30 +0100

    TEST/  ADDED TO ISSUES

--- Source/main.c
+++ Source/main.c
@@ -22,7 +22,7 @@ int main(int argc, const char * argv[]){
 
         frameTime = SDL_GetTicks() - frameStart;
         if(frameDelta > frameTime) SDL_Delay(frameDelta - frameTime);
-        else printf("\033[1;33mPerformances Issue : %d ms frame processing time excess.\033[0m\n", frameTime - frameDelta);
+        else printf("\033[1;33mPerformances IssueS : %d ms frame processing time excess.\033[0m\n", frameTime - frameDelta);
     }
     game->clean();
     return 0;
