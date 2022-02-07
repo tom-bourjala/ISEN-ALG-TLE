78ff1a3c87206c68fe6dac903ffb1c41e3c656f7
Tom Bourjala <tom.bourjala@gmail.com>
2022-02-07 23:39:38 +0100

    added ifndef statement for renderAddons

--- Source/Game/rendererAddons.h
+++ Source/Game/rendererAddons.h
@@ -4,6 +4,9 @@
 #include <SDL2/SDL.h>
 #include "game.h"
 
+#ifndef RENDERADDONS_H
+#define RENDERADDONS_H
+
 void DrawCircle(SDL_Renderer * renderer, int32_t centreX, int32_t centreY, int32_t radius);
 
 typedef enum {DO_Hit} debugObjectType;
@@ -16,3 +19,5 @@ typedef struct{
 } debugObject;
 
 GameObject *newGameObject_Debug(Game *GAME, int xpos, int ypos, int TTL, debugObjectType type);
+
+#endif
