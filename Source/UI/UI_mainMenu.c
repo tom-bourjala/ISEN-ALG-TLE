08da86e0b192c4112c332d3d226049a32b6752be
Tom Bourjala <tom.bourjala@gmail.com>
2022-04-09 17:33:13 +0200

    first intialisation of main menu logic

--- Source/UI/UI_mainMenu.c
+++ Source/UI/UI_mainMenu.c
@@ -14,10 +14,11 @@ static UI_button *sandboxButton = NULL;
 static UI_button *backButton = NULL;
 
 int A_RNULL(void *GAME){return 0;}
-int getx_middle(void *GAME){Game *game = GAME; return game->winWidth/2 + 500;}
-int gety_middle(void *GAME){Game *game = GAME; return game->winHeight/2 + 500;}
+int DIAG_X(void *GAME){return 240;}
+int getx_middle(void *GAME){Game *game = GAME; return game->winWidth/2;}
+int gety_middle(void *GAME){Game *game = GAME; return game->winHeight/2;}
 
-const int menuInBetween = 100;
+static const int menuInBetween = 150;
 
 int gety_top(void *GAME){return gety_middle(GAME) - menuInBetween;}
 int gety_bottom(void *GAME){return gety_middle(GAME) + menuInBetween;}
@@ -25,12 +26,49 @@ int gety_bottom(void *GAME){return gety_middle(GAME) + menuInBetween;}
 int gety_firstInRow(void *GAME){return gety_middle(GAME) - (2 * menuInBetween);}
 int gety_secondInRow(void *GAME){return gety_middle(GAME) - menuInBetween;}
 int gety_thirdInRow(void *GAME){return gety_middle(GAME);}
-int gety_bottomOfRow(void *GAME){return gety_middle(GAME) + (2*menuInBetween);}
+int gety_bottomOfRow(void *GAME){return gety_middle(GAME) + menuInBetween;}
 
 void onClickPrintf(void *self){
     printf("CLICK ON %p\n", self);
 }
 
+void showPlayMenu(){
+    playButton->hidden = true;
+    settingsButton->hidden = true;
+    quitButton->hidden = true;
+    // aboutButton->hidden = true;
+}
+
+void hidePlayMenu(){
+    playButton->hidden = false;
+    settingsButton->hidden = false;
+    quitButton->hidden = false;
+    // aboutButton->hidden = false;
+}
+
+void showHomeMenu(){
+    campaignButton->hidden = true;
+    endlessButton->hidden = true;
+    sandboxButton->hidden = true;
+    backButton->hidden = true;
+}
+
+void hideHomeMenu(){
+    campaignButton->hidden = false;
+    endlessButton->hidden = false;
+    sandboxButton->hidden = false;
+    backButton->hidden = false;
+}
+
+void switchToPlayMenu(void *self){
+    showPlayMenu();
+    hideHomeMenu();
+}
+
+void switchToHomeMenu(void *self){
+    showHomeMenu();
+    hidePlayMenu();
+}
 
 static UI_textureObject *menuBackground = NULL;
 char **(*LM_getTradById)(char *idToGet) = NULL;
@@ -47,12 +85,10 @@ void rescaleBackground(){
         menuBackground->rect.w = w * hRatio;
         menuBackground->rect.h = h * hRatio;
     }
-    // printf("%d, %d", getx_middle(THIS_GAME), gety_middle(THIS_GAME));
 }
 
 void onUpdate(){
     rescaleBackground();
-    //printf("%d,%d,%d,%d\n",playButton->actionArea->rect.x,playButton->actionArea->rect.y,playButton->actionArea->rect.w,playButton->actionArea->rect.h);
 }
 
 void UI_initMainMenu(void *GAME)
@@ -66,29 +102,30 @@ void UI_initMainMenu(void *GAME)
 
     UI_anchor *A_NULL = UI_newAnchor(game->menu, A_RNULL, A_RNULL);
 
-    UI_anchor *AH_PLAY = UI_newAnchor(game->menu, A_RNULL, gety_top);
-    UI_anchor *AH_SETTINGS = UI_newAnchor(game->menu, A_RNULL, gety_middle);
-    UI_anchor *AH_QUIT = UI_newAnchor(game->menu, A_RNULL, gety_bottom);
-    UI_anchor *AH_MIDLE = UI_newAnchor(game->menu, getx_middle, gety_middle);
+    UI_anchor *AH_PLAY = UI_newAnchor(game->menu, DIAG_X, gety_top);
+    UI_anchor *AH_SETTINGS = UI_newAnchor(game->menu, DIAG_X, gety_middle);
+    UI_anchor *AH_QUIT = UI_newAnchor(game->menu, DIAG_X, gety_bottom);
     // UI_anchor *AH_ABOUT = UI_newAnchor(game->menu, ?, ?);
 
-    UI_anchor *AP_CAMPAIGN = UI_newAnchor(game->menu, A_RNULL, gety_firstInRow);
-    UI_anchor *AP_ENDLESS = UI_newAnchor(game->menu, A_RNULL, gety_secondInRow);
-    UI_anchor *AP_SANDBOX = UI_newAnchor(game->menu, A_RNULL, gety_thirdInRow);
-    UI_anchor *AP_BACK = UI_newAnchor(game->menu, A_RNULL, gety_bottomOfRow);
-
-    //Main menu buttons
-    playButton = UI_newButton(THIS_GAME->menu, LM_getTradById("menu_home_play"), UI_BT_MAIN, AH_MIDLE, false, onClickPrintf, NULL, NULL, 3);
-    // settingsButton = UI_newButton(THIS_GAME->menu, LM_getTradById("menu_home_settings"), UI_BT_MAIN, AH_SETTINGS, false, onClickPrintf, NULL, NULL, 3);
-    // quitButton = UI_newButton(THIS_GAME->menu, LM_getTradById("menu_home_quit"), UI_BT_MAIN, AH_QUIT, false, onClickPrintf, NULL, NULL, 3);
-    // aboutButton = UI_newButton(THIS_GAME->menu, LM_getTradById("menu_home_about"), UI_BT_MAIN, AH_ABOUT, false, onClickPrintf, NULL, NULL, 3);
-
-    //Play menu buttons
-    // campaignButton = UI_newButton(THIS_GAME->menu, LM_getTradById("menu_home_campagin"), UI_BT_MAIN, AP_CAMPAIGN, false, onClickPrintf, NULL, NULL, 3);
-    // endlessButton = UI_newButton(THIS_GAME->menu, LM_getTradById("menu_home_endless"), UI_BT_MAIN, AP_ENDLESS, false, onClickPrintf, NULL, NULL, 3);
-    // sandboxButton = UI_newButton(THIS_GAME->menu, LM_getTradById("menu_home_sandbox"), UI_BT_MAIN, AP_SANDBOX, false, onClickPrintf, NULL, NULL, 3);
-    // backButton = UI_newButton(THIS_GAME->menu, LM_getTradById("menu_back"), UI_BT_BACK, AP_BACK, false, onClickPrintf, NULL, NULL, 3);
+    UI_anchor *AP_CAMPAIGN = UI_newAnchor(game->menu, DIAG_X, gety_firstInRow);
+    UI_anchor *AP_ENDLESS = UI_newAnchor(game->menu, DIAG_X, gety_secondInRow);
+    UI_anchor *AP_SANDBOX = UI_newAnchor(game->menu, DIAG_X, gety_thirdInRow);
+    UI_anchor *AP_BACK = UI_newAnchor(game->menu, DIAG_X, gety_bottomOfRow);
 
     menuBackground = UI_newStaticTextureObject(game->menu, (SDL_Rect) {0,0,1920,1080}, A_NULL, "menu_background.png");
-    //menuBackground->hidden = true;
+    // menuBackground->hidden = true;
+
+    //Main menu buttons
+    playButton = UI_newButton(THIS_GAME->menu, LM_getTradById("menu_home_play"), UI_B_BIG, AH_PLAY, false, switchToPlayMenu, NULL, NULL, 2);
+    settingsButton = UI_newButton(THIS_GAME->menu, LM_getTradById("menu_home_settings"), UI_B_BIG, AH_SETTINGS, false, onClickPrintf, NULL, NULL, 2);
+    quitButton = UI_newButton(THIS_GAME->menu, LM_getTradById("menu_home_quit"), UI_B_BIG, AH_QUIT, false, onClickPrintf, NULL, NULL, 2);
+    // aboutButton = UI_newButton(THIS_GAME->menu, LM_getTradById("menu_home_about"), UI_B_BIG, AH_ABOUT, false, onClickPrintf, NULL, NULL, 3);
+
+    //Play menu buttons
+    campaignButton = UI_newButton(THIS_GAME->menu, LM_getTradById("menu_home_campagin"), UI_B_BIG, AP_CAMPAIGN, false, onClickPrintf, NULL, NULL, 2);
+    endlessButton = UI_newButton(THIS_GAME->menu, LM_getTradById("menu_home_endless"), UI_B_BIG, AP_ENDLESS, false, onClickPrintf, NULL, NULL, 2);
+    sandboxButton = UI_newButton(THIS_GAME->menu, LM_getTradById("menu_home_sandbox"), UI_B_BIG, AP_SANDBOX, false, onClickPrintf, NULL, NULL, 2);
+    backButton = UI_newButton(THIS_GAME->menu, LM_getTradById("menu_back"), UI_B_BACK, AP_BACK, false, switchToHomeMenu, NULL, NULL, 1.5);
+
+    switchToHomeMenu(NULL);
 }
