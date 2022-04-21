012e3e280f31cd9cdbf50d1a8381ec2e181b35b0
Tom Bourjala <tom.bourjala@gmail.com>
2022-04-21 15:30:17 +0200

    demonstration of core system

--- Source/Game/gameManager.c
+++ Source/Game/gameManager.c
@@ -6,10 +6,13 @@
 #include "../UI/UI_loading.h"
 #include "../UI/UI_mainMenu.h"
 #include "../UI/UI_hud.h"
+#include "../Core/core.h"
 
 void launchMainMenu(void *GAME){
     UI_initMainMenu(GAME);
 }
+
+static GameObject *Core = NULL;
 
 void launchEndlessMode(void *GAME){
     Game *game = GAME;
@@ -21,6 +24,14 @@ void launchEndlessMode(void *GAME){
     float progress = 0.0f;
     launchLoading(GAME, &progress, "Loading Endless Mode");
     game->mapManager->loadMap("debug", 1920, 1080, &progress);
+    Core = newGameObject_Core(game, game->mapManager->currentMap->end, 100, 100, 10, 5);
+    appendInList(game->gameObjects, Core);
     endLoading(GAME);
     UI_initHud(GAME);
+}
+
+void updateGameManager(){
+    if(Core){
+        hitCore(Core, 1);
+    }
 }
