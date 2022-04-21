fc9bbcfb723e1c23878d07d6b45cd5d6db222b22
Tom Bourjala <tom.bourjala@gmail.com>
2022-04-21 14:23:14 +0200

    append of new GOT_Core in GameObjecType enum

--- Source/Game/game.h
+++ Source/Game/game.h
@@ -47,7 +47,7 @@ typedef struct
 Game *initGame(const char* title, int width, int height, bool fullscreen);
 
 typedef struct GameObject GameObject;
-typedef enum {GOT_Turret, GOT_Robot, GOT_Projectile, GOT_Effect, GOT_DEBUG} GameObjectType;
+typedef enum {GOT_Turret, GOT_Robot, GOT_Projectile, GOT_Effect, GOT_Core, GOT_DEBUG} GameObjectType;
 
 struct GameObject{
     void (*update)(void *self);
