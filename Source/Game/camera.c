f362ecb75c9e7175bc0c253a2e9ba2090c298a8c
Tom Bourjala <tom.bourjala@gmail.com>
2022-04-22 22:54:08 +0200

    camera now support rescale

--- Source/Game/camera.c
+++ Source/Game/camera.c
@@ -7,17 +7,24 @@ void initCamera(Game *game)
     GAME = game;
 }
 
+static void updateRectDest(SDL_Rect *dest){
+    dest->x -= GAME->cameraX;
+    dest->y -= GAME->cameraY;
+    dest->x *= GAME->cameraScale;
+    dest->y *= GAME->cameraScale;
+    dest->w *= GAME->cameraScale;
+    dest->h *= GAME->cameraScale;
+}
+
 void cameraRender(SDL_Texture *tex, SDL_Rect dest){
-    dest.x -= GAME->cameraX;
-    dest.y -= GAME->cameraY;
+    updateRectDest(&dest);
     SDL_RenderCopy(GAME->renderer, tex, NULL, &dest);
 }
 
 void cameraRenderFrame(SDL_Texture *tex, SDL_Rect dest, int frame){
     int frameWidth, frameHeight;
     SDL_QueryTexture(tex, NULL, NULL, &frameWidth, &frameHeight);
-    dest.x -= GAME->cameraX;
-    dest.y -= GAME->cameraY;
+    updateRectDest(&dest);
     SDL_Rect src = {frame * frameHeight, 0, frameHeight, frameHeight};
     SDL_RenderCopy(GAME->renderer, tex, &src, &dest);
 }
@@ -25,9 +32,8 @@ void cameraRenderFrame(SDL_Texture *tex, SDL_Rect dest, int frame){
 void cameraRenderEx(SDL_Texture *tex, SDL_Rect dest, int frame, float angle, bool hFlip, bool vFlip){
     int frameWidth, frameHeight;
     SDL_QueryTexture(tex, NULL, NULL, &frameWidth, &frameHeight);
+    updateRectDest(&dest);
     SDL_Rect src = {frame * frameHeight, 0, frameHeight, frameHeight};
-    dest.x -= GAME->cameraX;
-    dest.y -= GAME->cameraY;
     SDL_RendererFlip flip = SDL_FLIP_NONE;
     if(hFlip) flip = SDL_FLIP_HORIZONTAL;
     if(vFlip){
--- Source/Game/gameManager.c
+++ Source/Game/gameManager.c
@@ -8,26 +8,29 @@
 #include "../UI/UI_hud.h"
 #include "../Core/core.h"
 
-void launchMainMenu(void *GAME){
-    UI_initMainMenu(GAME);
+static Game *GAME = NULL;
+ 
+void launchMainMenu(void *game){
+    GAME = game;
+    UI_initMainMenu(game);
 }
 
 static GameObject *Core = NULL;
 
-void launchEndlessMode(void *GAME){
-    Game *game = GAME;
-    Selection *selection = game->selection;
+void launchEndlessMode(void *game){
+    GAME = game;
+    Selection *selection = GAME->selection;
     int mapId = 0;
     if(selection->type == SELECT_MAP){
         mapId = selection->selected.mapId;
     }
     float progress = 0.0f;
     launchLoading(GAME, &progress, "Loading Endless Mode");
-    game->mapManager->loadMap("debug", 1920, 1080, &progress);
-    Core = newGameObject_Core(game, game->mapManager->currentMap->end, 100, 100, 10, 5);
-    appendInList(game->gameObjects, Core);
+    GAME->mapManager->loadMap("debug", 1920, 1080, &progress);
+    Core = newGameObject_Core(GAME, GAME->mapManager->currentMap->end, 100, 100, 10, 5);
     endLoading(GAME);
     UI_initHud(GAME);
+    GAME->cameraScale = 0.7f;
 }
 
 void updateGameManager(){
