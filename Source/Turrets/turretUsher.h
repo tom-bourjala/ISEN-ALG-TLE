8cf43c990b4bfd6a24b6ae14685faf7403113b4f
HOARAU Rémi <remiette15@gmail.com>
2022-05-25 22:37:08 +0200

    comments and clean-up for turretUsher.h

--- Source/Turrets/turretUsher.h
+++ Source/Turrets/turretUsher.h
@@ -4,6 +4,12 @@
 #include "../Game/game.h"
 #include "../Game/camera.h"
 
+/** @brief renders the turret selection depending on if it can be placed or not regarding its position and cost
+    @param game the current game
+*/
 void renderTurretSelection(Game *game);
-void updateTurretSelection(Game *game);
+/** @brief reacts to the users input by blocking the placement of a turret if the conditions are not met
+    @param game the current game
+    @param event the user's input event
+*/
 void handleInputTurretSelection(Game *game, SDL_Event *event);
